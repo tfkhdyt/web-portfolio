014eecdd7e20ed788d30ef708857f61e0b717d43
tfkhdyt <ta142fa@gmail.com>
2022-03-15 13:20:42 +0700

    format and lint

--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -64,9 +64,10 @@ const ProjectCard = ({
             <Link href={repoLink}>
               <a
                 //  data-theme='dracula'
-                className={`group btn btn-outline btn-xs mt-1.5 umami--click--${name.replaceAll(' ', '-')}-repo-link ${
-                  isButtonClickable || 'pointer-events-none'
-                }`}
+                className={`group btn btn-outline btn-xs mt-1.5 umami--click--${name.replaceAll(
+                  ' ',
+                  '-'
+                )}-repo-link ${isButtonClickable || 'pointer-events-none'}`}
                 target='_blank'
               >
                 <svg
