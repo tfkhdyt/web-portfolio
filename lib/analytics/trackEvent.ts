315a744d612075259f673da5876ef95f90dee0cd
tfkhdyt <ta142fa@gmail.com>
2022-03-16 05:21:21 +0700

    remove data type from eventType parameter

--- lib/analytics/trackEvent.ts
+++ lib/analytics/trackEvent.ts
@@ -1,4 +1,4 @@
-export const trackEvent = (eventName: string, eventType: string = 'click') => {
+export const trackEvent = (eventName: string, eventType = 'click') => {
   if (window.umami && typeof window.umami.trackEvent === 'function') {
     window.umami.trackEvent(eventName, eventType)
   }
