45305186855bc899202cbccb69b499174d0ef05f
tfkhdyt <ta142fa@gmail.com>
2022-03-23 07:36:12 +0700

    revert to standard umami script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,17 +34,17 @@ export default class MyDocument extends Document {
           <link rel='manifest' href='/manifest.json' />
           <link rel='apple-touch-icon' href='/icon-512x512.png' />
           <meta name='theme-color' content='#3B82F6' />
-          {/* <script
+          <script
             async
             defer
             data-website-id='285223d1-eb6c-4b6e-a2a2-c0ca69ce122c'
             src='https://umami.tfkhdyt.my.id/umami.js'
-            /> */}
-          <Script 
+          />
+          {/* <Script 
             data-website-id='285223d1-eb6c-4b6e-a2a2-c0ca69ce122c'
             src='https://umami.tfkhdyt.my.id/umami.js'
             strategy='lazyOnload'
-          />
+            /> */}
         </Head>
         <body className='font-body'>
           <Main />
