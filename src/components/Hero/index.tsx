4b14d7034b45654ff03b73dfcc3ab6f697177b67
tfkhdyt <tfkhdyt@pm.me>
2022-04-24 06:36:16 +0700

    fix mouse scroll icon in hero not visible in light mode

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -102,12 +102,14 @@ const Hero = () => {
               <path
                 id='wheel'
                 d='M123.359,79.775l0,72.843'
-                className={`[fill:none] [stroke:#fff] [stroke-width:20px] ${styles.wheel}`}
+                className={`[fill:none] [stroke-width:20px] ${styles.wheel}`}
+                stroke='currentColor'
               />
               <path
                 id='mouse'
                 d='M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z'
-                className='[fill:none] [stroke:#fff] [stroke-width:20px]'
+                className='[fill:none] [stroke-width:20px]'
+                stroke='currentColor'
               />
             </svg>
 
