fa67d0d03d8591dba8b597510eb0e524080dc9e4
tfkhdyt <tfkhdyt@pm.me>
2022-05-03 11:40:23 +0700

    fix SWR

--- src/components/Footer/Visitors.tsx
+++ src/components/Footer/Visitors.tsx
@@ -1,26 +1,24 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import useSWR from 'swr';
 
-const fetcher = async (url: string) => {
-  const { data } = await axios.get(url, {
-    headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_UMAMI_TOKEN}`,
-    },
-  });
-  return data;
-};
-
 const Visitors = () => {
-  const { data, error } = useSWR<{ pageviews: { value: number } }>(
-    `https://umami.tfkhdyt.my.id/api/websites?start_at=1&end_at=${Date.now()}`,
+  const fetcher = async (url: string) => {
+    return axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_UMAMI_TOKEN}`,
+        },
+      })
+      .then((res) => res.data);
+  };
+  const { data, error } = useSWR(
+    `https://umami.tfkhdyt.my.id/api/website/1/stats?start_at=1&end_at=${new Date().getTime()}`,
     fetcher
   );
 
-  useEffect(() => {
-    if (error) console.error(error);
-  }, []);
-  // const totalVisitors = 1000;
+  if (error) console.log(error);
+  if (!data) return 'Loading';
 
   return (
     <p className='text-xs font-semibold text-slate-600'>
