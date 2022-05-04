530e50a9d1341414cb5801bed92aaba136caac9b
tfkhdyt <tfkhdyt@pm.me>
2022-05-04 17:08:16 +0700

    disable total visitors counter

--- src/components/Footer/Visitors.tsx
+++ src/components/Footer/Visitors.tsx
@@ -1,23 +1,54 @@
-import axios from 'axios';
+// import axios from 'axios';
+import { GetServerSideProps } from 'next';
 // import { useEffect } from 'react';
-import useSWR from 'swr';
+// import useSWR from 'swr';
 
-const Visitors = () => {
-  const fetcher = async (url: string) => {
-    return axios
-      .get(url, {
-        headers: {
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_UMAMI_TOKEN}`,
-        },
-      })
-      .then((res) => res.data);
-  };
-  const { data, error } = useSWR(
-    `https://umami.tfkhdyt.my.id/api/website/1/stats?start_at=1&end_at=${new Date().getTime()}`,
-    fetcher
-  );
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const res = await fetch(
+//     `https://umami.tfkhdyt.my.id/api/website/1/stats?start_at=1&end_at=${new Date().getTime()}`,
+//     {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${process.env.NEXT_PUBLIC_UMAMI_TOKEN}`,
+//       },
+//     }
+//   );
+//   const data = await res.json();
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// };
 
-  if (error) console.log(error);
+interface IValue {
+  value: number;
+}
+
+interface IPageViews {
+  pageviews: IValue;
+}
+
+interface IVisitorsProps {
+  data: IPageViews;
+}
+
+const Visitors = ({ data }: IVisitorsProps) => {
+  // const fetcher = async (url: string) => {
+  //   return axios
+  //     .get(url, {
+  //       headers: {
+  //         Authorization: `Bearer ${process.env.NEXT_PUBLIC_UMAMI_TOKEN}`,
+  //       },
+  //     })
+  //     .then((res) => res.data);
+  // };
+  // const { data, error } = useSWR(
+  //   `https://umami.tfkhdyt.my.id/api/website/1/stats?start_at=1&end_at=${new Date().getTime()}`,
+  //   fetcher
+  // );
+  //
+  // if (error) console.log(error);
 
   return (
     <div className='flex items-center text-xs font-semibold text-slate-600'>
