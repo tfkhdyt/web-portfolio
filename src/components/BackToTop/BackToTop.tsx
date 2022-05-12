56fd57b5b86767de8c8a61ed0128e03e4085b74b
tfkhdyt <tfkhdyt@pm.me>
2022-05-12 15:29:51 +0700

    fix back to top button position in desktop

--- src/components/BackToTop/BackToTop.tsx
+++ src/components/BackToTop/BackToTop.tsx
@@ -12,7 +12,7 @@ export function BackToTop() {
   );
 
   return (
-    <div className='fixed bottom-8 right-8'>
+    <div className='fixed bottom-8 right-8 lg:right-12'>
       <button
         className={`inline-block p-3 bg-base-100 text-blue-500 border border-blue-500 rounded-full hover:text-white hover:bg-blue-500 active:bg-blue-400 focus:outline-none focus:ring transition-all duration-500 ease-in-out 
         ${
