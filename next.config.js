cc6ba70d4fc446bcd18beca3e2191dfd63955dfc
tfkhdyt <tfkhdyt@pm.me>
2022-05-12 08:03:13 +0700

    turn on again remove console config

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   reactStrictMode: true,
   compiler: {
-    // removeConsole: {
-    //   exclude: ['error'],
-    // },
+    removeConsole: {
+      exclude: ['error'],
+    },
   },
   swcMinify: true,
   pwa: {
