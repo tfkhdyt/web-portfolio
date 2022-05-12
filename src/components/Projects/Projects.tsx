c356070f6279a6a469ebd9b79d0f4ca3d8163616
tfkhdyt <tfkhdyt@pm.me>
2022-05-12 15:39:00 +0700

    Add 3 grid cols for desktop

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -59,7 +59,7 @@ const Projects = () => {
               ))}
             </motion.div>
             {/* content */}
-            <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
+            <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
               <AnimatePresence exitBeforeEnter>
                 {projects
                   .filter((value) => value.type === activeTab)
