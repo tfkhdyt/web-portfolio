b8c972c565bfce6d6054f7149f379ce2f725e763
tfkhdyt <tfkhdyt@pm.me>
2022-06-28 20:48:42 +0700

    fix(download cv button): change bg color

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -68,7 +68,7 @@ const Hero = () => {
               </div>
               <Link href='https://drive.google.com/file/d/1NJcXYr5SZNkxt03KKaiDNNYoc2_cCWBj/view?usp=sharing'>
                 <a
-                  className='flex justify-center items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 mt-6 space-x-2 w-fit'
+                  className='flex justify-center items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 mt-6 space-x-2 w-fit'
                   target='_blank'
                 >
                   <svg
