d45941d5eccee4d76a62d577773d5e5f56193b5d
tfkhdyt <tfkhdyt@pm.me>
2022-07-02 18:07:02 +0700

    fix(metatags): add www to link

--- src/components/MetaTags/data.ts
+++ src/components/MetaTags/data.ts
@@ -2,7 +2,7 @@ export default {
   header: 'TAUFIK HIDAYAT - Portfolio Website',
   title: 'TAUFIK HIDAYAT - Portfolio Website',
   favicon: '/favicon.ico',
-  link: 'https://tfkhdyt.my.id',
+  link: 'https://www.tfkhdyt.my.id',
   deskripsi:
     "Hi everyone, my name is Taufik Hidayat. I'm a Full Stack Developer, Informatics Student, YouTuber, Free/Libre and Open Source Software Enthusiast, and Professional Googler.",
   thumbnail: '/thumbnail-720.png',
