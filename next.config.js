575bd29873c85d9f6e461f42fecc49cb0d6de298
tfkhdyt <tfkhdyt@pm.me>
2022-08-23 12:07:54 +0700

    fix(next.config.js): disable pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
+/* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
 // require('dotenv').config()
 
-module.exports = withPWA({
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   compiler: {
+//     removeConsole: {
+//       exclude: ['error'],
+//     },
+//   },
+//   swcMinify: true,
+//   pwa: {
+//     dest: 'public',
+//     disable: process.env.NODE_ENV === 'development',
+//   },
+// });
+
+module.exports = {
   reactStrictMode: true,
   compiler: {
     removeConsole: {
@@ -10,8 +25,4 @@ module.exports = withPWA({
     },
   },
   swcMinify: true,
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
-});
+};
